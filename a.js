5ff4af196bd41209244c37eec70f6440e572a801
Van Pho Nguyen <vanpho02@gmail.com>
2018-05-13 09:11:43 +0700

    a.js

---
+++ a.js
@@ -0,0 +1,12 @@
+const numbers = [1, 3, 5, 6, 2, 4, 9];
+
+const people = [
+    { name: 'Teo', age: 10, height: 100 },
+    { name: 'Ti', age: 17, height: 120 },
+    { name: 'Tun', age: 13, height: 180 },
+    { name: 'Tuan', age: 15, height: 110 },
+];
+
+const arr = numbers.map(x => x * x);
+
+console.log(arr);
